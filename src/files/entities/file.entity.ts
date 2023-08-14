dc4b1d5b3a044666a367c1460a1ef0609a916159
Romartiny <romortiny@gmail.com>
2023-08-14 14:39:53 +0300

    create entity for files and users + create uploader for files

--- src/files/entities/file.entity.ts
+++ src/files/entities/file.entity.ts
@@ -1,1 +1,27 @@
-export class File {}
+import {Column, DeleteDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn} from "typeorm";
+import {UserEntity} from "../../users/entities/user.entity";
+
+@Entity('files')
+export class FileEntity {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    fileName: string;
+
+    @Column()
+    originalName: string;
+
+    @Column()
+    size: number;
+
+    @Column()
+    mimeType: string;
+
+    @ManyToOne(() => UserEntity, user => user.files)
+    user: UserEntity;
+
+    @DeleteDateColumn()
+    deletedAt: Date;
+}
