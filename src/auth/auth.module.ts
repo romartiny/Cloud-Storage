3fbaac769fb2e92b9291a8d74b13a12928295d1d
Romartiny <romortiny@gmail.com>
2023-08-14 19:06:53 +0300

    implementing strategies and jwt guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,11 +4,25 @@ import {AuthService} from './auth.service';
 import {UsersModule} from "../users/users.module";
 import {PassportModule} from "@nestjs/passport";
 import {LocalStrategy} from "./strategies/local.strategy";
+import {JwtModule} from "@nestjs/jwt";
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import {JwtStrategy} from "./strategies/jwt.strategy";
 
 @Module({
     controllers: [AuthController],
-    providers: [AuthService, LocalStrategy],
-    imports: [UsersModule, PassportModule]
+    providers: [AuthService, LocalStrategy, JwtStrategy],
+    imports: [
+        JwtModule.registerAsync({
+           imports: [ConfigModule],
+           inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => {
+               return {
+                   secret: configService.get('SECRET_KEY'),
+                   signOptions: { expiresIn: configService.get('EXPIRES_IN')},
+               }
+            }
+        }),
+        UsersModule, PassportModule]
 })
 export class AuthModule {
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,19 @@
 import {ForbiddenException, Injectable} from '@nestjs/common';
 import {UsersService} from "../users/users.service";
 import {CreateUserDto} from "../users/dto/create-user.dto";
+import {UserEntity} from "../users/entities/user.entity";
+import {JwtService} from "@nestjs/jwt";
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UsersService) {}
+    constructor(private usersService: UsersService, private jwtService: JwtService) {
+    }
 
     async validateUser(email: string, password: string): Promise<any> {
         const user = await this.usersService.findByEmail(email);
 
         if (user && user.password === password) {
-            const { password, ...result} = user;
+            const {password, ...result} = user;
             return result;
         }
 
@@ -19,11 +22,23 @@ export class AuthService {
 
     async register(dto: CreateUserDto) {
         try {
-            return await this.usersService.create(dto);
+            const userData = await this.usersService.create(dto);
+
+            return {
+                token: this.jwtService.sign({
+                    id: userData.id,
+                })
+            }
         } catch (err) {
             console.log(err);
 
             throw new ForbiddenException('Error creating user');
+        }
+    }
+
+    async login(user: UserEntity) {
+        return {
+            token: this.jwtService.sign({id: user.id})
         }
     }
 }
